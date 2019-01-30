e2d82d77cb5f668bbd2f483dbcac2b4f78243f1e
Polina Gannotskaya <gannotsk21@up.edu>
2019-01-29 19:08:36 -0800

    better Working
    changed some small things

--- js/index.js
+++ js/index.js
@@ -1,7 +1,7 @@
 //this will use your function to write information into the index.html
-a = prompt('Enter the first number');
-b = prompt('Enter the second number');
-answer = addition(a,b);
-var myHeading = document.querySelector('h1');
-myHeading.textContent = answer;
-
+a = 2;//prompt('Enter the first number');
+b = 2;//prompt('Enter the second number');
+function sum() {
+	document.getElementById("id").innerHTML = addition(a,b);
+}
+sum();
